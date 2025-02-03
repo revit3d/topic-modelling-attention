d46fb56c6c105eeec4ac753a05ed4cdca9ed97e5
revit3d <tumanezhikov@yandex.ru>
2025-02-03 19:39:19 +0300

    Vectorize coherence metric

--- src/cartm/metrics/coherence.py
+++ src/cartm/metrics/coherence.py
@@ -22,9 +22,7 @@ class CoherenceMetric(Metric):
             tag = self.__class__.__name__
         super().__init__(tag=tag)
 
-        data = data.T  # (W, D)
-        self.word_doc_indicator = data.astype(bool)
-        self.word_co_occurence = self.word_doc_indicator @ self.word_doc_indicator.T  # (W, W)
+        self.word_doc_indicator = (data > 0).astype(int).T  # (W, D)
         self.word_occurence = self.word_doc_indicator.sum(axis=1)  # (W, )
         self.top_k = top_k
         self._eps = eps
@@ -35,19 +33,21 @@ class CoherenceMetric(Metric):
             kth=-self.top_k,
             axis=0,
         )[-self.top_k:]  # (W_k, T)
-        n_topics = phi_wt.shape[1]
-        coherence = []
+        n_docs = self.word_doc_indicator.shape[1]
 
-        for t in range(n_topics):
-            top_words = sorted(
-                top_words_per_topic[t],
-                key=lambda w: -phi_wt[w, t],
-                reverse=True,
-            )  # (W_k, )
-            co_occurrences = self.word_co_occurence[top_words, top_words]  # (W_k, W_k)
-            occurrences = self.word_occurence[top_words]  # (W_k, )
-            pmi = jnp.log(
-                co_occurrences / occurrences[:, None] / occurrences[None, :] + self._eps
-            )  # (W_k, W_k)
-            coherence.append(jnp.triu(pmi, k=1).mean())
-        return jnp.mean(coherence)
+        top_words_per_topic = top_words_per_topic.T  # (T, W_k)
+        top_word_indicator = self.word_doc_indicator[top_words_per_topic]  # (T, W_k, D)
+        top_word_indicator_T = top_word_indicator.transpose(0, 2, 1)  # (T, D, W_k)
+
+        co_occurrences = top_word_indicator @ top_word_indicator_T  # (T, W_k, W_k)
+        co_occurrences /= n_docs  # normalize probabilities
+        occurrences = self.word_occurence[top_words_per_topic]   # (T, W_k)
+        occurrences /= n_docs  # normalize probabilities
+        pmi = jnp.log(
+            co_occurrences / occurrences[..., None] / occurrences[:, None, :] + self._eps
+        )  # (T, W_k, W_k)
+
+        unique_pmis = jnp.triu(pmi, k=1)  # (T, W_k, W_k)
+        n_pairs = self.top_k * (self.top_k - 1) // 2
+        coherence_per_topic = unique_pmis.sum(axis=(1, 2)) / n_pairs  # (T, )
+        return jnp.mean(coherence_per_topic)
